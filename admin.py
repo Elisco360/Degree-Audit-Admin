f4125e62e2564be4aadc0c3c3df4e046803b84d6
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2023-07-13 14:09:54 +0000

    updates to data editor changes and student submissions

--- admin.py
+++ admin.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+import time
 import streamlit as st
 from deta import Deta
 import yaml
 from streamlit_authenticator import Authenticate
 from yaml.loader import SafeLoader
+import pandas as pd
 
 st.set_page_config(page_icon='favicon.ico', page_title='Admin Portal', layout='wide')
 
@@ -12,29 +14,32 @@ deta = Deta(st.secrets['data_key'])
 students_data = deta.Base('submitted')
 students_files = deta.Drive('audit-files')
 
+def update_details(update_data):
+    for k in update_data.items():
+        students_data.update(k[1], k[0])
+    st.success('Student details updated')
 
 def main():
     _info = students_data.fetch().items
 
     st.markdown("<h1 style='text-align: center;'>ADMIN PORTAL</h1>", unsafe_allow_html=True)
     st.markdown('\n\n\n\n')
-    update, download = st.tabs(['Update Student Attendance', 'Download Degree Audits'])
-
-    def update_attendance(user_key):
-        students_data.update({'Attended Session': True}, user_key)
+    update, download = st.tabs(['Student List', 'Download Degree Audits'])
 
     with update:
         st.data_editor(_info, column_order=('Name', 'Major', 'Date', 'Attended Session 1', 'Attended Session 2', 'Attended Session 3', 'Notes'),
-                       disabled=['Name', 'Major', 'Date'], key='edited_data', use_container_width=True)
-        try:
-            _altered = _info[int(list(st.session_state['edited_data']['edited_rows'].keys())[0])]
-            if _altered['Attended Session']:
-                students_data.update({'Attended Session': False}, _altered['key'])
-            else:
-                students_data.update({'Attended Session': True}, _altered['key'])
+                       disabled=['Name', 'Major', 'Date'], key='data_editor', use_container_width=True)
+        
+        val = list(st.session_state["data_editor"]['edited_rows'].items())
 
-        except:
-            pass
+        new_data = dict()
+        df = pd.DataFrame(_info)
+        for i in val:
+            row = df.iloc[i[0]]
+            new_data[row['key']] = i[1]
+            update_details(new_data)
+
+        st.metric('Students Submissions', len(_info))
 
     with download:
         students = students_files.list()['names']
